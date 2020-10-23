c63774501ab2beb884e545a8c815a357ea4291f1
mattcbodily <mattcbodily@gmail.com>
2020-10-23 15:37:14 -0600

    server running

---
+++ db/seed.sql
@@ -0,0 +1,13 @@
+create table if not exists users (
+    user_id serial primary key,
+    full_name varchar(100) not null,
+    email varchar(150) not null,
+    password varchar(250) not null,
+    tutor boolean not null
+);
+
+create table if not exists tutor_student_junction (
+    junction_id serial primary key,
+    tutor_id int references users(user_id) not null,
+    student_id int references users(user_id) not null
+);
---
+++ server/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express'),
+      session = require('express-session'),
+      massive = require('massive'),
+      {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+      app = express();
+
+app.use(express.json());
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SESSION_SECRET,
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 365}
+}));
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db => {
+    app.set('db', db);
+    console.log('db connected')
+    app.listen(SERVER_PORT, () => console.log(`Server running on ${SERVER_PORT}`));
+});
